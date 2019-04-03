ce25dbe07d76695614337e5f79155c6083455c6a
david <davdiaavi@gmail.com>
2019-04-03 14:51:25 +0200

    motor werkt goeie arduin code

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,16 +1,35 @@
 #include <Stepper.h>
 #define STEPS 2038 // the number of steps in one revolution of your motor (28BYJ-48)
 Stepper stepper(STEPS, 8, 10, 9, 11);
-//const int lockedPin = 3;
+const int lockedPin = 7;
+boolean locked = false;
+
 void setup() {
-  // nothing to do
+  stepper.setSpeed(4);
+  pinMode(lockedPin, INPUT);
+  Serial.begin(115200);
 }
 void loop() {
-   //Serial.println(digitalRead(lockedpin));
-   delay(2000);
-  stepper.setSpeed(3); // 1 rpm
-  stepper.step(500); // do 2038 steps -- corresponds to one revolution in one minute
-  delay(1000); // wait for one second
-//  stepper.setSpeed(3); // 6 rpm
-  stepper.step(-500); // do 2038 steps in the other direction with faster speed -- corresponds to one revolution in 10 seconds
+   Serial.print("Pin value: ");
+   Serial.println(digitalRead(lockedPin));
+   if(!locked){
+    if(digitalRead(lockedPin)==HIGH){
+      stepper.step(500);
+      locked = true;
+    }
+   }
+   if(locked){
+    if(digitalRead(lockedPin)==LOW){
+      stepper.step(-500);
+      locked=false;
+    }
+   }
+
+   delay(1000);
+
+  //stepper.setSpeed(3); // 1 rpm
+  //stepper.step(500); // do 2038 steps -- corresponds to one revolution in one minute
+  //delay(1000); // wait for one second
+  //stepper.setSpeed(3); // 6 rpm
+  //stepper.step(-500); // do 2038 steps in the other direction with faster speed -- corresponds to one revolution in 10 seconds
 }
